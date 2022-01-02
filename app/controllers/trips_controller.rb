aa452f72ebb1d7c3613c10591d4a5d076fee9914
Mallory Vining <mallorygallery12@gmail.com>
2022-01-02 16:27:04 -0700

    User Story 1-2

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,9 @@
+class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+
+  end
+end
---
+++ spec/features/trails/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Trails Show page" do
+  it 'shows the names of all trips' do
+    rocky = Trip.create!(name: "Rocky Mtn National Park")
+    yellowstone = Trip.create!(name: "Yellowstone National Park")
+    yosemite = Trip.create!(name: "Yosemite National Park")
+
+    visit "/trips/"
+    expect(page).to have_content(rocky.name)
+    expect(page).to have_content(yosemite.name)
+    expect(page).to have_content(yellowstone.name)
+  end
+
+  it 'links to show page' do
+    rocky = Trip.create!(name: "Rocky Mtn National Park")
+    yellowstone = Trip.create!(name: "Yellowstone National Park")
+    yosemite = Trip.create!(name: "Yosemite National Park")
+
+    visit "/trips/"
+    click_link rocky.name
+    expect(current_path).to eq("/trips/#{rocky.id}")
+  end
+end
